68ca6123c618502ec1ad49d2e8adbb5b19d8966c
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-05-05 21:50:05 +0100

    food choice component created

---
+++ src/components/Guestfoodchoice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Guestfoodchoice = () => {
+  return (
+    <div>
+        <h3>Here are the food choices</h3>
+    </div>
+  )
+}
+
+export default Guestfoodchoice
