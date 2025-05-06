634a13263ddaf652efe5979a540b90512d98ae7d
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-05-06 16:21:08 +0100

    project firestore imported and state created

--- src/components/Songlist.jsx
+++ src/components/Songlist.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { projectfirestore } from '../firebase/config';
 
-const Songlist
- = () => {
+const Songlist = () => {
+    const [data, setData] = useState(null);
+    const [pending, setPending] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+
+    }, [])
   return (
     <div>
         <h3>This is the song list</h3>
