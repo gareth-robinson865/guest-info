62a000977b342d1996c9d318f3c764ba39b477eb
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-05-06 15:29:13 +0100

    drinkslist and songlist components imported to main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 
 import './App.css'
+import DrinksList from './components/DrinksList'
 import Guestfoodchoice from './components/Guestfoodchoice'
 import Guestlist from './components/Guestlist'
 import Header from './components/Header'
+import Songlist from './components/Songlist'
 
 function App() {
   
 
   return (
     <>
-      
-        
       <h1>Vite + React</h1>
       <Header />
       <Guestlist />
       <Guestfoodchoice />
-      
+      <Songlist />
+      <DrinksList />
     </>
   )
 }
