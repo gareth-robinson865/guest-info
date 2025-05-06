e309058393b2886f0f94d3523e89055a9fad5309
gareth-robinson865 <gazz_1998@hotmail.co.uk>
2025-05-06 16:36:53 +0100

    useEffect and some of the component created but need to check how to do it when not exported to another component

--- src/components/Songlist.jsx
+++ src/components/Songlist.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { projectfirestore } from '../firebase/config';
 
 const Songlist = () => {
     const [data, setData] = useState(null);
     const [pending, setPending] = useState(false);
     const [error, setError] = useState(false);
-    useEffect(() => {
 
-    }, [])
+    useEffect(() => {
+        setPending(true);
+        projectfirestore.collection('Guests').get().then((snapshot) => {
+            console.log(snapshot); 
+        })
+    }, []);
+
   return (
     <div>
         <h3>This is the song list</h3>
+        {error && <p className="error">{error}</p>}
+        {pending && <p className="loading">Loading...</p>}
+        
     </div>
   )
 }
